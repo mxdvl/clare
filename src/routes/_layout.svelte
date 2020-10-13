26a877596d8d53367825f2bba4f52667f1212dde
Max Duval <max@mxdvl.com>
2020-10-13 21:17:14 +0100

    initial commit

---
+++ src/routes/_layout.svelte
@@ -0,0 +1,25 @@
+<script>
+  import Nav from "../components/Nav.svelte";
+
+  export let segment;
+</script>
+
+<style>
+  main {
+    position: relative;
+    max-width: 54rem;
+    background-color: white;
+    padding: 2em;
+    margin: 0 auto;
+    box-sizing: border-box;
+
+    font-family: Cambria, Cochin, Georgia, Times, "Times New Roman", serif;
+    background-image: url("/ricepaper_v3.png");
+  }
+</style>
+
+<Nav {segment} />
+
+<main>
+  <slot />
+</main>
---
+++ src/routes/about.svelte
@@ -0,0 +1,7 @@
+<svelte:head>
+  <title>Writings</title>
+</svelte:head>
+
+<h1>About this site</h1>
+
+<p>Hey there, my name’s Clare.</p>
