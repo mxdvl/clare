1e894ada3a3d783d6dc36de1b26de3f1fb68fc6d
Max Duval <max@mxdvl.com>
2020-10-14 09:14:20 +0100

    renamed blog to words

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -65,6 +65,6 @@
 
     <!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
 		     the blog data when we hover over the link or tap it on a touchscreen -->
-    <li><a rel=prefetch aria-current="{segment === 'writing' ? 'page' : undefined}" href="writing">writing</a></li>
+    <li><a rel=prefetch aria-current="{segment === 'words' ? 'page' : undefined}" href="words">words</a></li>
   </ul>
 </nav>
