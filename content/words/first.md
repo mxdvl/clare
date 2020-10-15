43a0d7b36154e7ac6c1135af3ebfc74ee71b0e5d
Max Duval <max@mxdvl.com>
2020-10-15 08:52:59 +0100

    add marked md parsing

--- content/words/first.md
+++ content/words/first.md
@@ -4,4 +4,24 @@ title: First poem
 
 This is interesting!
 
-Something something bean…
+Something something _bean…_
+
+```
+evil code
+```
+
+| 1    |2    | 3   |
+|------|-----|-----|
+| A    | B   | C   |
+
+## Second title
+
+> Blockquote
+
+## A nice list 
+
+- Apple
+- Juice
+- Eggs
+- Nothing
+- Ever lasts
--- src/routes/words/[slug].json.js
+++ src/routes/words/[slug].json.js
@@ -2,6 +2,7 @@ import fs from "fs";
 import path from "path";
 
 import grayMatter from "gray-matter";
+import marked from 'marked';
 
 const getPost = (slug) =>
 	fs.readFileSync(path.resolve("content", "words" , `${slug}.md`), "utf-8");
@@ -12,13 +13,14 @@ export function get(req, res, next) {
 	const { slug } = req.params;
 
 	const { content, data } = grayMatter(getPost(slug))
+	const html = marked(content);
 
 	if (content) {
 		res.writeHead(200, {
 			'Content-Type': 'application/json'
 		});
 
-		res.end(JSON.stringify({slug, html: content, ...data}));
+		res.end(JSON.stringify({slug, html, ...data}));
 	} else {
 		res.writeHead(404, {
 			'Content-Type': 'application/json'
