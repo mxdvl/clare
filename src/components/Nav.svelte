035995c8fc983076df5aec7d7c9329b107de715b
Max Duval <max@mxdvl.com>
2020-10-17 22:00:15 +0100

    reorganise content folders

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -79,7 +79,7 @@
 
 		<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
 				 the blog data when we hover over the link or tap it on a touchscreen -->
-		{#each ['words', 'snaps', 'notes'] as folder}
+		{#each ['words'] as folder}
 			<li>
 				<a
 					rel="prefetch"
