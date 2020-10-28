f921c3388129e936cfba0a48efff82716544ec4d
Max Duval <max@mxdvl.com>
2020-10-28 22:16:19 +0000

    export logos with sharp

---
+++ src/sharp.js
@@ -0,0 +1,19 @@
+import sharp from "sharp";
+import path from "path";
+
+const folder = "static";
+
+const CANVAS = 32;
+const DENSITY = 72;
+
+const sizes = [192, 512];
+
+sizes.map((size) =>
+	sharp(path.resolve(folder, "favicon.svg"), {
+		density: DENSITY * (size / CANVAS),
+	})
+		.resize(size, size)
+		.toFile(path.resolve(folder, `logo-${size}.png`), function (err) {
+			if (err) console.log("Something went wrong", err);
+		})
+);
