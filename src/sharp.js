31f3c63acf5481043517216487403476726f6f16
Max Duval <max.duval@guardian.co.uk>
2020-11-01 13:30:38 +0000

    Automated favicons (#2)
    * live in the fast lane
    
    * fix to a version number
    
    * better closed logic
    
    * add dates
    
    * handle single pages
    
    * add more favicon sizes
    
    * rename portrait mural
    
    * add apple-touch-icon
    
    * remove unused Sacramento font
    
    * add tabs to favicon

--- src/sharp.js
+++ src/sharp.js
@@ -6,14 +6,18 @@ const folder = "static";
 const CANVAS = 32;
 const DENSITY = 72;
 
-const sizes = [32, 192, 512];
+const sizes = [16, 32, 64, 128, 180, 192, 256, 512];
 
-sizes.map((size) =>
+sizes.map((size) => {
+	const file = `notebook-${size}.png`;
 	sharp(path.resolve(folder, "favicon.svg"), {
 		density: DENSITY * (size / CANVAS),
 	})
-		.resize(size, size)
-		.toFile(path.resolve(folder, `notebook-${size}.png`), function (err) {
-			if (err) console.log("Something went wrong", err);
+		.toFile(path.resolve(folder, file))
+		.then((info) => {
+			console.log(`${file} - ${info.size} bits`);
 		})
-);
+		.catch((err) => {
+			if (err) console.log("Something went wrong", err);
+		});
+});
