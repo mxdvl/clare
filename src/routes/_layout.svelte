8431ed1a6512d01ceee92e1fbcae47db449922ae
Max Duval <max.duval@guardian.co.uk>
2020-11-01 21:21:12 +0000

    Add analytics (#4)
    * live in the fast lane
    
    * fix to a version number
    
    * better closed logic
    
    * add dates
    
    * handle single pages
    
    * add more favicon sizes
    
    * rename portrait mural
    
    * add apple-touch-icon
    
    * and remove unused Sacramento font
    
    * add tabs to favicon
    
    * update segment
    
    * add fathom analytics

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -3,6 +3,7 @@
 	import Nav from "../components/Nav.svelte";
 
 	export let segment: string;
+	export const isClosed = (_segment: string) => _segment === undefined;
 </script>
 
 <style>
@@ -22,17 +23,47 @@
 	}
 
 	#notebook.closed {
-		max-width: 24rem;
+		width: min(24rem, 100%);
 	}
 
-	:global(#notebook.closed .cover) {
-		z-index: 3;
+	main {
+		position: relative;
+		grid-area: page;
+
+		color: var(--text);
+		background-color: var(--paper);
+		border: 2px solid;
+		border-radius: 0 1rem 1rem 0;
+		padding: 1rem;
+		padding-bottom: 1rem;
+		box-sizing: border-box;
+		line-height: 1rem;
+
+		font-size: 0.8rem;
+
+		background-image: repeating-linear-gradient(
+			var(--lines),
+			var(--lines) var(--line),
+			var(--paper) var(--line),
+			var(--paper) calc(1rem)
+		);
+		background-repeat: no-repeat;
+		background-position: 1rem 7rem;
+		background-size: calc(100% - 2rem);
+
+		box-shadow: 4px 4px 0 -2px var(--paper), 4px 4px 0 var(--text),
+			-4px 8px 0 -2px var(--paper), -4px 8px 0 var(--text),
+			0 12px 0 -2px var(--paper), 0 12px 0 var(--text);
 	}
 </style>
 
-<div id="notebook" class:closed={segment === undefined}>
+<div id="notebook" class:closed={isClosed(segment)}>
 	<Nav {segment} />
-	<Cover />
+	<Cover closed={isClosed(segment)} />
 
-	<slot />
+	{#if !isClosed(segment)}
+		<main class="page">
+			<slot />
+		</main>
+	{/if}
 </div>
