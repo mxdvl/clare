ea07778aacda4a9ab6c19da9a6cfa93db3646f54
Max Duval <max.duval@guardian.co.uk>
2020-12-14 21:37:56 -0500

    fix rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,66 +1,77 @@
-import resolve from '@rollup/plugin-node-resolve';
-import replace from '@rollup/plugin-replace';
-import commonjs from '@rollup/plugin-commonjs';
-import svelte from 'rollup-plugin-svelte';
-import babel from '@rollup/plugin-babel';
-import { terser } from 'rollup-plugin-terser';
-import sveltePreprocess from 'svelte-preprocess';
-import typescript from '@rollup/plugin-typescript';
-import config from 'sapper/config/rollup.js';
-import pkg from './package.json';
+import resolve from "@rollup/plugin-node-resolve";
+import replace from "@rollup/plugin-replace";
+import commonjs from "@rollup/plugin-commonjs";
+import svelte from "rollup-plugin-svelte";
+import babel from "@rollup/plugin-babel";
+import { terser } from "rollup-plugin-terser";
+import sveltePreprocess from "svelte-preprocess";
+import typescript from "@rollup/plugin-typescript";
+import config from "sapper/config/rollup.js";
+import pkg from "./package.json";
 
 const mode = process.env.NODE_ENV;
-const dev = mode === 'development';
+const dev = mode === "development";
 const legacy = !!process.env.SAPPER_LEGACY_BUILD;
 
 const onwarn = (warning, onwarn) =>
-	(warning.code === 'MISSING_EXPORT' && /'preload'/.test(warning.message)) ||
-	(warning.code === 'CIRCULAR_DEPENDENCY' && /[/\\]@sapper[/\\]/.test(warning.message)) ||
-	(warning.code === 'THIS_IS_UNDEFINED') ||
+	(warning.code === "MISSING_EXPORT" && /'preload'/.test(warning.message)) ||
+	(warning.code === "CIRCULAR_DEPENDENCY" &&
+		/[/\\]@sapper[/\\]/.test(warning.message)) ||
+	warning.code === "THIS_IS_UNDEFINED" ||
 	onwarn(warning);
 
 export default {
 	client: {
-		input: config.client.input().replace(/.js$/, '.ts'),
+		input: config.client.input().replace(/.js$/, ".ts"),
 		output: config.client.output(),
 		plugins: [
 			replace({
-				'process.browser': true,
-				'process.env.NODE_ENV': JSON.stringify(mode)
+				"process.browser": true,
+				"process.env.NODE_ENV": JSON.stringify(mode),
 			}),
 			svelte({
-				dev,
-				hydratable: true,
 				preprocess: sveltePreprocess(),
-				emitCss: true
+				emitCss: true,
+				compilerOptions: {
+					dev,
+					hydratable: true,
+				},
 			}),
 			resolve({
 				browser: true,
-				dedupe: ['svelte']
+				dedupe: ["svelte"],
 			}),
 			commonjs(),
 			typescript({ sourceMap: dev }),
 
-			legacy && babel({
-				extensions: ['.js', '.mjs', '.html', '.svelte'],
-				babelHelpers: 'runtime',
-				exclude: ['node_modules/@babel/**'],
-				presets: [
-					['@babel/preset-env', {
-						targets: '> 0.25%, not dead'
-					}]
-				],
-				plugins: [
-					'@babel/plugin-syntax-dynamic-import',
-					['@babel/plugin-transform-runtime', {
-						useESModules: true
-					}]
-				]
-			}),
+			legacy &&
+				babel({
+					extensions: [".js", ".mjs", ".html", ".svelte"],
+					babelHelpers: "runtime",
+					exclude: ["node_modules/@babel/**"],
+					presets: [
+						[
+							"@babel/preset-env",
+							{
+								targets: "> 0.25%, not dead",
+							},
+						],
+					],
+					plugins: [
+						"@babel/plugin-syntax-dynamic-import",
+						[
+							"@babel/plugin-transform-runtime",
+							{
+								useESModules: true,
+							},
+						],
+					],
+				}),
 
-			!dev && terser({
-				module: true
-			})
+			!dev &&
+				terser({
+					module: true,
+				}),
 		],
 
 		preserveEntrySignatures: false,
@@ -72,42 +83,46 @@ export default {
 		output: config.server.output(),
 		plugins: [
 			replace({
-				'process.browser': false,
-				'process.env.NODE_ENV': JSON.stringify(mode)
+				"process.browser": false,
+				"process.env.NODE_ENV": JSON.stringify(mode),
 			}),
 			svelte({
-				generate: 'ssr',
-				hydratable: true,
 				preprocess: sveltePreprocess(),
-				dev
+				compilerOptions: {
+					generate: "ssr",
+					hydratable: true,
+					dev,
+				},
 			}),
 			resolve({
-				dedupe: ['svelte']
+				dedupe: ["svelte"],
 			}),
 			commonjs(),
-			typescript({ sourceMap: dev })
+			typescript({ sourceMap: dev }),
 		],
-		external: Object.keys(pkg.dependencies).concat(require('module').builtinModules),
+		external: Object.keys(pkg.dependencies).concat(
+			require("module").builtinModules
+		),
 
-		preserveEntrySignatures: 'strict',
+		preserveEntrySignatures: "strict",
 		onwarn,
 	},
 
 	serviceworker: {
-		input: config.serviceworker.input().replace(/.js$/, '.ts'),
+		input: config.serviceworker.input().replace(/.js$/, ".ts"),
 		output: config.serviceworker.output(),
 		plugins: [
 			resolve(),
 			replace({
-				'process.browser': true,
-				'process.env.NODE_ENV': JSON.stringify(mode)
+				"process.browser": true,
+				"process.env.NODE_ENV": JSON.stringify(mode),
 			}),
 			commonjs(),
 			typescript({ sourceMap: dev }),
-			!dev && terser()
+			!dev && terser(),
 		],
 
 		preserveEntrySignatures: false,
 		onwarn,
-	}
+	},
 };
