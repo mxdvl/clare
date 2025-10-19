5e0134bada35a27f1875989d4ba73632a4fff111
Max Duval <hi@mxdvl.com>
2025-10-19 13:11:42 +0100

    feat: integrate Pages CMS for homepage

---
+++ src/content/home.md
@@ -0,0 +1,22 @@
+# Nice to meet you. 👋
+
+**Senior User Researcher Consultant working at [Opencast](https://opencastsoftware.com/).**
+
+5 years experience working as a user researcher in the public and private sector.
+
+Having previously worked extensively in education and abroad, my experience spans a breadth of cultures and age groups. I bring a wide skill set to the user researcher role. People have always been at the centre of my work. A strong advocate of user centred design and passionate about creating services that include and enable users.
+
+## Read some of my recent posts:
+
+- [How a user-centred approach is improving principles for sustainable services](https://opencastsoftware.com/insights/blog/2025/march/how-a-user-centred-approach-is-improving-principles-for-sustainable-services/) March 2025
+- [Why we must all mind the digital exclusion gap](https://opencastsoftware.com/insights/blog/2023/october/why-we-must-all-mind-the-digital-exclusion-gap/) Oct 2023
+- [Top considerations before conducting user research | UX Collective](https://uxdesign.cc/top-considerations-before-conducting-user-research-2e8c24e9f793) Sep 2022
+- [Using Trello for User Research | UX Collective](https://uxdesign.cc/using-trello-for-user-research-d767ce0eb37c) May 2022
+- [Why must design teams consider the growing digital divide in the UK? | Sopra Steria](https://medium.com/sopra-steria-uk-design-team/why-must-design-teams-consider-the-growing-digital-divide-in-the-uk-50aaddbd672f) Feb 2022
+- [Reviewing the ‘user experience’ of language learning in schools](https://clarelwelch.medium.com/reviewing-the-user-experience-of-language-learning-in-schools-cafc355edea0) Jul 2021
+
+If you want to get in touch, it’s [clarelwelch@gmail.com](mailto:clarelwelch@gmail.com) or you can find me [on LinkedIn](http://linkedin.com/in/clare-welch).
+
+In my free time, I love socialising, writing, watching a good series or documentary, reading a book I can get lost in, completing a crossword (or at least giving it a go), planning my next adventure, dancing to Lady Gaga, trying new foods and eating pizza.
+
+Way too much pizza. 🍕
--- src/routes/(page)/+page.svelte
+++ src/routes/(page)/+page.svelte
@@ -1,101 +1,32 @@
+<script lang="ts">
+  import { marked } from 'marked';
+  import homeMarkdown from '../../content/home.md?raw';
+
+  const homeHtml = marked.parse(homeMarkdown);
+</script>
+
 <svelte:head>
   <title>Clare Welch: User Researcher</title>
 </svelte:head>
 
-<h1>Nice to meet you. 👋</h1>
-
-<p class="bold">
-  Senior User Researcher Consultant working at <a
-    href="https://opencastsoftware.com/">Opencast</a
-  >.
-</p>
-
-<p>
-  5 years experience working as a user researcher in the public and private
-  sector.
-</p>
-
-<p>
-  Having previously worked extensively in education and abroad, my experience
-  spans a breadth of cultures and age groups. I bring a wide skill set to the
-  user researcher role. People have always been at the centre of my work. A
-  strong advocate of user centred design and passionate about creating services
-  that include and enable users.
-</p>
-
-<h2>Read some of my recent posts:</h2>
-
-<ul>
-  <li>
-    <a
-      href="https://opencastsoftware.com/insights/blog/2025/march/how-a-user-centred-approach-is-improving-principles-for-sustainable-services/"
-      >How a user-centred approach is improving principles for sustainable
-      services
-    </a> March 2025
-  </li>
-  <li>
-    <a
-      href="https://opencastsoftware.com/insights/blog/2023/october/why-we-must-all-mind-the-digital-exclusion-gap/"
-      >Why we must all mind the digital exclusion gap</a
-    > Oct 2023
-  </li>
-  <li>
-    <a
-      href="https://uxdesign.cc/top-considerations-before-conducting-user-research-2e8c24e9f793"
-      >Top considerations before conducting user research | UX Collective</a
-    > Sep 2022
-  </li>
-  <li>
-    <a href="https://uxdesign.cc/using-trello-for-user-research-d767ce0eb37c"
-      >Using Trello for User Research | UX Collective</a
-    > May 2022
-  </li>
-  <li>
-    <a
-      href="https://medium.com/sopra-steria-uk-design-team/why-must-design-teams-consider-the-growing-digital-divide-in-the-uk-50aaddbd672f"
-      >Why must design teams consider the growing digital divide in the UK? |
-      Sopra Steria</a
-    > Feb 2022
-  </li>
-  <li>
-    <a
-      href="https://clarelwelch.medium.com/reviewing-the-user-experience-of-language-learning-in-schools-cafc355edea0"
-      >Reviewing the ‘user experience’ of language learning in schools</a
-    > Jul 2021
-  </li>
-</ul>
-
-<p>
-  If you want to get in touch, it’s <a href="mailto:clarelwelch@gmail.com"
-    >clarelwelch@gmail.com</a
-  >
-  or you can find me
-  <a href="http://linkedin.com/in/clare-welch">on LinkedIn</a>.
-</p>
-
-<p>
-  In my free time, I love socialising, writing, watching a good series or
-  documentary, reading a book I can get lost in, completing a crossword (or at
-  least giving it a go), planning my next adventure, dancing to Lady Gaga,
-  trying new foods and eating pizza.
-</p>
-
-<p>Way too much pizza. 🍕</p>
+<div class="page" aria-label="Homepage content">
+  {@html homeHtml}
+</div>
 
 <style>
-  h2 {
+  :global(.page h2) {
     font-size: 1em;
     line-height: 1rem;
     padding-bottom: 1rem;
   }
 
-  ul {
+  :global(.page ul) {
     margin: 0;
     padding: 0;
     padding-left: 1rem;
   }
 
-  li {
+  :global(.page li) {
     padding-bottom: 1rem;
   }
 </style>
